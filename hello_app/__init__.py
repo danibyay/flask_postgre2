50db80b8e82359fa105cb245441c2287a4d49247
Daniela Becerra Gonzalez <danybecerr@gmail.com>
2020-06-16 21:11:34 -0500

    Add new route, models and db session access

--- hello_app/__init__.py
+++ hello_app/__init__.py
@@ -1,2 +1,7 @@
 from flask import Flask  # Import the Flask class
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)    # Create an instance of the class for our use
+app.config.from_object('config.DevelopmentConfig')
+db = SQLAlchemy(app)
--- hello_app/views.py
+++ hello_app/views.py
@@ -1,8 +1,35 @@
 from datetime import datetime
-from flask import Flask, render_template
-from . import app
+from flask import Flask, render_template, session, request, redirect, url_for
+from . import app, db
+from .models import Attendee
+import logging 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route('/Registration', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        attendee = Attendee()
+        attendee.first_name = request.form['first_name']
+        attendee.last_name = request.form['last_name']
+        attendee.email = request.form['email']
+        attendee.job_position = request.form['job_position']
+        attendee.company = request.form['company']
+        attendee.city = request.form['city']
+        attendee.state = request.form['state']
+        attendee.interests = request.form['interest']
+        attendee.comments = request.form['message']
+        attendee.conference_id = app.config.get('CONFERENCE_ID')
+        
+        try:
+            db.session.add(attendee)
+            db.session.commit()
+            session['message'] = 'Thank you, {} {}, for registering!'.format(attendee.first_name, attendee.last_name)
+            return redirect('/Registration')
+        except:
+            logging.error('Error occured while saving your information')
+    
+    return render_template('registration.html')
+ 
